da4c28259f60f0a905790c6c5fb780499900abf2
drapkinamaria <drapkinamaria02@gmail.com>
2023-04-14 01:12:47 +0500

    try2.1

--- solution.py
+++ solution.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
+from statsmodels.stats.weightstats import ztest
 
-chat_id = 760034497 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+chat_id = 532761772 # Ваш chat ID, не меняйте название переменной
+
+def solution(x) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 0.06
-    t_stat, p_value = ttest_1samp(x, 300)
-    return p_value/2 < alpha and t_stat < 0 # Ваш ответ, True или False
+    p = 0.02
+    return ztest(x, value=300, alternative='smaller')[1] <= p # Ваш ответ, True или False
